a869c6a134a3ae001466d6b7d3272fe3a94a766d
Marcus Gigandet <78104059+marcusgigandet@users.noreply.github.com>
2026-09-15 13:38:50 -0400

    cleanup: Cleanup files and documentation.

--- Project_2/src/main.cpp
+++ Project_2/src/main.cpp
@@ -1,19 +1,17 @@
-/*
- * Copyright (C) 2026 Marcus Gigandet
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+/// Copyright (C) 2026 Marcus Gigandet
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU General Public License as published by
+/// the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License
+/// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <Arduino.h>
 #include <cmath>
@@ -124,6 +122,7 @@ void setup()
 /// @brief Handles the button presses for the standalone button.
 ///
 /// This handles any debouncing before confirming the button press.
+/// This also logs the button press duration (press after debouncing to release) to serial comm.
 void handleButtonPress()
 {
 	const uint8_t  rawButtonState{static_cast<uint8_t>(digitalRead(Pins::BUTTON))};
@@ -207,8 +206,8 @@ void handleTone(const uint8_t n)
 {
 	const uint32_t frequency{440 * static_cast<uint32_t>(pow(2, ((12 * octave + n - 49)) / 12.0))};
 
-	// Ignore invalid frequencies
-	if (0 < frequency)
+	// Ignore invalid frequencies and n-values
+	if ((0 != frequency) && (0 != n))
 	{
 		tone(Pins::SPEAKER, frequency, TONE_DURATION_MS);
 	}
@@ -217,11 +216,5 @@ void handleTone(const uint8_t n)
 void loop()
 {
 	handleButtonPress();
-
-	uint8_t n{getKeypadPress()};
-
-	if (0 < n)
-	{
-		handleTone(n);
-	}
+	handleTone(getKeypadPress());
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# CEG-4330-Coursework
+
+This repository contains all labs from CEG-4330. The labs use an ESP-32 microcontroller and platformio.
